550a07d43374ca1ccc27e6946e6d662152890d52
anphongdoa5 <anphongdoa5@gmail.com>
2026-03-19 13:34:30 +0700

    7.2 done

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/baongoc', function () {
@@ -15,3 +15,4 @@ Route::get('/dangkhoa', [App\Http\Controllers\ControllerKhoa::class, 'dangKhoa']
 Route::get('/thuthao', [App\Http\Controllers\ControllerThao::class, 'thuThao']);
 Route::get('/minhnguyet', [App\Http\Controllers\ControllerNguyet::class, 'minhNguyet']);
 
+Route::get('/top-10-phim', [App\Http\Controllers\Top10Controller::class, 'top10bophim']);
