ad7622ee8bb63d341998df2e173d95bc7d678196
HuyGia0527 <giahuy9756@gmail.com>
2026-03-19 12:32:41 +0700

    7.1 Đã xong

--- app/Http/Controllers/ControllerHuy.php
+++ app/Http/Controllers/ControllerHuy.php
@@ -1,9 +1,15 @@
 <?php
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
 
-class ControllerKhoa extends Controller{
+class ControllerHuy extends Controller{
     public function giaHuy()
     {
         return 'Phạm Bùi Gia Huy';
+    }
+
+    public function theLoaiPhim(){
+        $theloaiphim = DB::table('genre') -> select('genre_name', 'genre_name_vn') -> get();
+        return view('danhsachtheloaiphim', compact('theloaiphim'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,14 @@ Route::get('/chian', function () {
     return "Huỳnh Văn Chí An";
 });
 
+Route::get('/', function(){
+    return view('welcome');
+});
+
 Route::get('/giaHuy', [App\Http\Controllers\ControllerHuy::class, 'giaHuy']);
 Route::get('/dangkhoa', [App\Http\Controllers\ControllerKhoa::class, 'dangKhoa']);
 
 Route::get('/thuthao', [App\Http\Controllers\ControllerThao::class, 'thuThao']);
 Route::get('/minhnguyet', [App\Http\Controllers\ControllerNguyet::class, 'minhNguyet']);
+Route::get('/theloai', [App\Http\Controllers\ControllerHuy::class, 'theLoaiPhim']);
 
