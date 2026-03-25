a20f917762149d8bc70b3872a495ce20ac2296ac
dangkhoa9999 <030239230091@st.buh.edu.vn>
2026-03-25 22:33:35 +0700

    khoa gui layout

--- resources/views/components/book-detail-layout.blade.php
+++ resources/views/components/book-detail-layout.blade.php
@@ -1,2 +1,37 @@
 <x-book-layout>
-</x-book-layout>
+    <x-slot name="title">
+        Chi tiết sách
+    </x-slot>
+
+    <div class="container mt-4">
+        <div class="row">
+            <!-- Ảnh bìa -->
+            <div class="col-md-4 text-center">
+                <img src=""
+                     class="img-fluid shadow" 
+                     style="max-height: 350px;">
+            </div>
+
+            <!-- Thông tin -->
+            <div class="col-md-8">
+                <h4 class="font-weight-bold">
+                </h4>
+
+                <p><strong>Nhà cung cấp:</strong> </p>
+                <p><strong>Nhà xuất bản:</strong> </p>
+                <p><strong>Tác giả:</strong> </p>
+                <p><strong>Hình thức:</strong></p>
+
+                <p class="text-danger font-weight-bold" style="font-size: 20px;">
+                </p>
+            </div>
+        </div>
+
+        <!-- Mô tả -->
+        <div class="mt-4">
+            <h5 class="font-weight-bold">Mô tả:</h5>
+            <p style="text-align: justify;">
+            </p>
+        </div>
+    </div>
+</x-book-layout>    
